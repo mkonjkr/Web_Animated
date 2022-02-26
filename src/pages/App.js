3b756c5b57075e26a633d62dc2c9318d604d9d5d
unknown <mkonjkr@gmail.com>
2022-02-26 14:26:19 -0800

    Animate-text transform #1

---
+++ src/pages/App.js
@@ -0,0 +1,12 @@
+import "../styles/home.scss";
+import AnimatedText from "../components/AnimatedText.jsx";
+
+function App() {
+  return (
+    <div className="page-wrapper">
+      <AnimatedText text="testestest" />
+    </div>
+  );
+}
+
+export default App;
